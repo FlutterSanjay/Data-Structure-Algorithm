ec5e967306180f34b245839a568d848a8dc32eac
AIdevel <sanjayshaw80138@gmail.com>
2025-07-24 15:08:00 +0530

    Optimized Sort 0s,1s,2s of Linked List

---
+++ Linked_List/Singly_Linked-List/sort_0s_1s_2s_Optimized.cpp
@@ -0,0 +1,96 @@
+// Optimized Sort 0s,1s,2s
+// Pyaari Approach
+// TC -O(n) & SC - O(1)
+#include<bits/stdc++.h>
+using namespace std;
+
+class Node{
+public:
+int data;
+Node* next;
+
+Node(int data){
+  this->data=data;
+  next=NULL;
+  }
+};
+void insertionAtTail(Node* &head,Node* &tail,int data){
+  Node* temp=new Node(data);
+  if(tail==NULL){
+    head=temp;
+    tail=temp;
+    return;
+  }
+  tail->next=temp;
+  tail=temp;
+  tail->next=NULL;
+}
+
+
+// sort 0s,1s,2s
+Node* sortLL(Node* &head){
+  Node* zeroHead=new Node(-1);
+  Node* zeroTail=zeroHead;
+
+  Node* oneHead=new Node(-1);
+  Node* oneTail=oneHead;
+  
+  Node* twoHead=new Node(-1);
+  Node* twoTail=twoHead;
+
+
+  Node* curr=head;
+
+  while(curr!=NULL){
+      Node* temp=curr;
+    if(curr->data==0){
+      zeroTail->next=temp;
+      zeroTail=temp;
+     
+      
+    }else if(curr->data == 1){
+      oneTail->next=temp;
+        oneTail=temp;
+      
+      
+    }else if(curr->data == 2){
+        twoTail->next=temp;
+        twoTail=temp;
+       
+      
+    }
+    curr=curr->next;
+  }
+
+  zeroTail->next=oneHead->next;
+  oneTail->next=twoHead->next;
+  twoTail->next = NULL;
+  
+  return zeroHead->next;
+}
+
+void print(Node* &head){
+    Node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+int main(){  
+  Node* head=NULL;
+  Node* tail=NULL;
+  insertionAtTail(head,tail,1);
+  insertionAtTail(head,tail,2);
+  insertionAtTail(head,tail,2);
+  insertionAtTail(head,tail,0);
+  insertionAtTail(head,tail,0);
+  insertionAtTail(head,tail,0);
+  print(head);
+  cout<<endl;
+  Node* result=sortLL(head);
+  print(result);
+ 
+
+  return 0;
+}
