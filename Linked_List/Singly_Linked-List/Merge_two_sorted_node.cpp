491ca63ed3ca53ccc428135ac227f8cb9b2167bc
AIdevel <sanjayshaw80138@gmail.com>
2025-07-25 11:18:25 +0530

    Merge Two Sorted Linked List

--- Linked_List/Singly_Linked-List/Merge_two_sorted_node.cpp
+++ Linked_List/Singly_Linked-List/Merge_two_sorted_node.cpp
@@ -1,89 +1,129 @@
-// Merge Two sorted Node
-#include<iostream>
+// Merge Two Sorted List
+/*✅ List 1:
+
+1 -> 3 -> 5 -> 7 -> 9 -> 11 -> 13 -> 15
+✅ List 2:
+
+2 -> 3 -> 4 -> 6 -> 8 -> 10
+
+🧠 Expected Output:
+
+1 2 3 3 4 5 6 7 8 9 10 11 13 15
+*/
+#include<bits/stdc++.h>
 using namespace std;
 class Node{
-    public:
-    int val;
-    Node* next;
-    Node(int data){
-        val=data;
-        next=NULL;
-    }
+public:
+int data;
+Node* next;
+Node(int data){
+  this->data=data;
+  this->next=NULL;
+}
 };
 
-void insert(Node* &head,int data){
-    Node* temp=new Node(data);
-    temp->next=head;
-    head=temp;
+Node* mergeSortedLL(Node* &head1,Node* &head2){
+    if(head1==NULL){
+        return head2;
+    }
+    if(head2==NULL){
+        return head1;
+    }
+
+    // Step:1 create dummy Node
+    Node* ansHead=NULL;
+    Node* ansTail=NULL;
+
+    //Step2: merge Two LL
+
+    while(head1!=NULL && head2!=NULL){
+        if(head1->data <= head2->data){
+            Node* newNode = new Node(head1->data);
+            if(ansHead==NULL){
+                ansHead=newNode;
+                ansTail=newNode;
+            }else{
+                ansTail->next=newNode;
+                ansTail=newNode;
+            }
+            head1=head1->next;
+        }else{
+            Node* newNode=new Node(head2->data);
+            ansTail->next = newNode;
+            ansTail=newNode;
+            head2=head2->next;
+        }
+    }
+
+    // Ho skta hai ki First LL Null na huaa ho
+    while(head1!=NULL){
+        Node* newNode=new Node(head1->data);
+            ansTail->next = newNode;
+            ansTail=newNode;
+            head1=head1->next;
+    }
+
+     // Ho skta hai ki Second LL Null na huaa ho
+    while(head2!=NULL){
+        Node* newNode=new Node(head2->data);
+            ansTail->next = newNode;
+            ansTail=newNode;
+            head2=head2->next;
+    }
+
+    return ansHead;
 }
 
-Node* merge(Node* &head1,Node* &head2){
-   Node* dummy=new Node(-1);
-   Node* ptr1=head1;
-   Node* ptr2=head2;
-   Node* ptr3=dummy;
 
-   while(ptr1!=NULL && ptr2!=NULL){
-    if(ptr1->val<ptr2->val){
-        ptr3->next=ptr1;
-        ptr1=ptr1->next;
-    }
-    else{
-        ptr3->next=ptr2;
-        ptr2=ptr2->next;
-    }
-    ptr3=ptr3->next;
-   }
-   if(ptr1!=NULL){
-    ptr3->next=ptr1;
-   }
-   else{
-    ptr3->next=ptr2;
-   }
-   return dummy->next;
+
+void print(Node* &head){
+  Node* temp=head;
+  while(temp!=NULL){
+      cout<<temp->data<<" ";    
+      temp=temp->next;
+  }
 }
 
-void display(Node* head){
-    Node* temp=head;
-    while(temp!=NULL){
-        cout<<temp->val<<" ";
-        temp=temp->next;
-    }
-}
-void insert_tail(Node* &head,int data){
-    Node* temp=new Node(data);
-    Node* lnode=head;
 
-    while(lnode->next !=NULL){
-        lnode=lnode->next;
-    }
-    lnode->next=temp;
-}
 int main(){
-    Node* head1=NULL;
-    Node* head2=NULL;
+  Node* head1=new Node(1);
+  Node* second = new Node(3);
+  Node* third = new Node(5);
+  Node* fourth = new Node(7);
+  Node* fifth = new Node(9);
+  Node* sixth = new Node(11);
+  Node* seventh = new Node(13);
+  Node* eighth = new Node(15);
+  head1->next=second;
+  second->next=third;
+  third->next=fourth;
+  fourth->next=fifth;
+  fifth->next=sixth;
+  sixth->next=seventh;
+  seventh->next=eighth;
+  eighth->next=NULL;
+  
+  print(head1);
+  cout<<endl;
 
-    insert(head1,1);
-    int data;
-    for(int i=0;i<2;i++){
-        cout<<"Enter your no : ";
-        cin>>data;
-        insert_tail(head1,data);
-    }
-    display(head1);
+  Node* head2=new Node(2);
+  Node* second1 = new Node(3);
+  Node* third1 = new Node(4);
+  Node* fourth1 = new Node(6);
+  Node* fifth1 = new Node(8);
+  Node* sixth1 = new Node(10);
+  head2->next=second1;
+  second1->next=third1;
+  third1->next=fourth1;
+  fourth1->next=fifth1;
+  fifth1->next=sixth1;
+  sixth1->next=NULL;
+  
+  print(head2);
+  cout<<endl;
+  Node* result = mergeSortedLL(head1,head2);
+  print(result);
 
-    cout<< endl;
 
-    insert(head2,2);
-    
-    for(int i=0;i<2;i++){
-        cout<<"Enter your no : ";
-        cin>>data;
-        insert_tail(head2,data);
-    }
-    display(head2);
-    Node* temp=merge(head1,head2);
-    cout<<endl;
-    display(temp);
-    return 0;
+  return 0;
 }
