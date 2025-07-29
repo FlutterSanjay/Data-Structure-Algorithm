fb0494f5f95d510b95f11ee6c26715a03b236298
AIdevel <sanjayshaw80138@gmail.com>
2025-07-29 09:10:50 +0530

    Redundant Bracket

--- Stack/Redundant_bracket.cpp
+++ Stack/Redundant_bracket.cpp
@@ -1,55 +1,42 @@
-/* Redundant Bracket:-
-   A set of parenthesis is redundant if the same sub-expression is surrounded by unnecessary or multiple brackets. 
-
-example:-
-
-Input: str = “((a+b))”
-Output: YES
-Explanation: ((a+b)) can reduced to (a+b), this Redundant
-*/
-
-#include<iostream> // used for input and output purpose
-#include<stack> // used for using or implementing stack
-#include<string> // used for using string 
-#include<stdbool.h> // used for boolean character
+// Redundant Bracket
+#include<bits/stdc++.h>
 using namespace std;
 
-// This function check the string is redundant or not and return flase or true to calling place
-bool find(string &s){
-    stack<int> st;   // declaring stack
-    for(int i=0;i<s.length();i++){
-        char ch=s[i];   // store the each character in ch
+bool redundantBracket(string &s){
+    if(s==""){
+        return false;
+    }
+    stack<char> st;
+    bool op = false;
+    for (int i = 0; i < s.length();i++){
+        char ch = s[i];
 
-        if(ch=='('|| ch=='+'|| ch=='-'|| ch=='*' || ch=='/'){   // compare ch with this element
+        if(ch=='(' || ch =='+' || ch =='-' || ch == '*' || ch=='/'){
             st.push(ch);
-        }
-        else{
-            if(ch==')'){ // other wise check ending curly bracket
-                bool finds=true;     // assign true if ch found
-                while(st.top()!='('){
-                    char top=st.top();  // assign the top element of stack in top
+        }else if (ch == ')'){
+            op = false;
 
-                    if(top=='+' || top=='-' || top=='*' || top=='/'){ // comparing the top with the given character 
-                        finds=false;  // assign the false
-                    }
-                    st.pop();  // remove the last the element from stack
+            while(!st.empty() && st.top() != ')'){
+                if(st.top() == '+' || st.top() == '-' || st.top() == '*' || st.top() == '/'){
+                    op = true;
                 }
-                if(finds==true)  // check the given find variable matched with true or not
-                    return true; // match then send true
-
-                    st.pop();   // remove the last element from the stack
-                }
+                st.pop();
+            }
+            if(!st.empty()){
+                st.pop();
             }
         }
-        return false; // otherwise return false
+    }
+    if(op){
+        return true;
+    }else{
+        return false;
+    }
 }
 
+int main(){
+  string s = "(a+b))";
+  cout<<redundantBracket(s)<<endl;
 
-    int main(){
-        string str="((a+b))"; // given string
-       bool result= find(str);
-       cout<<result;          // if string is redundant then print 1
-                              // other wise print 0;
-       return 0;     
-        
-    }
+    return 0;
+}
