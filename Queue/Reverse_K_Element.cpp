315f01ba32548e19876a8d85435b41f9a7ddb026
AIdevel <sanjayshaw80138@gmail.com>
2025-08-04 13:01:37 +0530

    Reverse Queue and Reverse first k element

--- Queue/Reverse_K_Element.cpp
+++ Queue/Reverse_K_Element.cpp
@@ -1,52 +1,54 @@
-// reverse the K Element in Queue            // GeeksforGeeks
-#include<iostream>
-#include<queue>
-#include<stack>
+#include<bits/stdc++.h>
 using namespace std;
 
-queue<int> reverse(queue<int> q,int k){
-    stack<int> s;
-    // step-1 : push k Element from Q to stack
-    for(int i=0;i<k;i++){
-       int ans=q.front();
-       q.pop();
-       s.push(ans);
-    }
-    // step 2 : push all Stack Element in Q
-    while(!s.empty()){
-        int ans=s.top();
-        s.pop();
-        q.push(ans);
-    }
+
+void kReverse(queue<int>& q,int k){
     
-    // step 3 : fetch n-k Element from Q and push to Q
-    int t=q.size()-k;
-    while(t--){
-        int ans=q.front();
-        q.pop();
-        q.push(ans);
-    }
-    return q;
-}
-int main(){
-    queue<int> q;
-    int size,k,data;
-    cout<<"Enter the size of Queue  : ";
-    cin>>size;
-    for(int i=0;i<size;i++){
-        cout<<"Enter your no : ";
-        cin>>data;
-        q.push(data);
-    }
-    cout<<"Enter the Number of Element want to Rotate : ";
-    cin>>k;
-    q=reverse(q,k);
-    
-    // for output 
-    while(!q.empty()){
-        cout<<q.front()<<" ";
-        q.pop();
-    }
-    return 0;
+    if(q.size() < k || k<=0) return;
+    stack<int> st;
+
+  int i=0;
+  while(i<k){
+    st.push(q.front());
+    q.pop();
+    i++;
+  }
+
+  
+int t=st.size();
+  while(t--){
+    q.push(st.top());
+    st.pop();
+  }
+
+  i=0;
+  int n=q.size();
+  while(i< n-k){
+    q.push(q.front());
+    q.pop();
+    i++;
+  }
 }
 
+
+void printQueue(queue<int> &q){
+    int i=0;
+    while(!q.empty()){
+        cout<<q.front() <<" ";
+        q.pop();
+    }
+}
+int main(){
+
+  queue<int> q;
+  q.push(3);
+  q.push(6);
+  q.push(9);
+  q.push(12);
+  q.push(15);
+  int k=3;
+  kReverse(q,k);
+  printQueue(q);
+  
+  return 0;
+}
